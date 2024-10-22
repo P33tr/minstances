2f22e0819959064c297390365f93db139466ab7b
Peter Marshall <marshall.peter@gmail.com>
2024-10-22 20:29:06 +0100

    created

---
+++ minstances/minstances/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace minstances.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
