040cc56a717c76a0d59cb4f07da63b551be8ff18
Peter Marshall <marshall.peter@gmail.com>
2024-11-24 16:09:33 +0000

    added sqlite db to record searchs

---
+++ minstances/minstances/Data/IMinstancesRepository.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using minstances.Models;
+
+namespace minstances.Data
+{
+    public interface IMinstancesRepository
+    {
+        Task<ErrorOr<Search>> CreateSearch(string searchTerm);
+    }
+}
